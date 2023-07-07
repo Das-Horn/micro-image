af8be94988fecac3e7ffb3de56e099589a539214
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2023-07-07 13:27:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+
+
+function App() {
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default App;
