e6806d74d5c55883b980c8f759cd7c76d702fb37
cd <craigdoyle220301+git@gmail.com>
2023-07-07 14:50:27 +0100

    Basic form implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,51 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 
 
+let image;
+
+
 function App() {
+
+
+  async function draw_image() {
+    const canvas = document.getElementById("canv") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d");
+    const imageData = document.getElementById("fileIn") as HTMLInputElement;
+  
+    if(imageData.files === null) {
+      return
+    }
+
+    image = await createImageBitmap(imageData.files[0]);
+
+
+    let modWidth = canvas.width / image.width;
+    let modHeight = canvas.height / image.height;
+
+    console.log(canvas.height / image.height);
+    ctx?.clearRect(0, 0, canvas.width, canvas.height);
+    ctx?.drawImage(image, 0, 0, image.width * modWidth, image.height * modHeight);
+  }
 
   return (
     <div className="container">
-      
+      <nav>
+        <h1>Image Converter</h1>
+      </nav>
+      <div id="Cont">
+        <canvas id="canv"></canvas>
+        <div id="formCont">
+          <input type="file" onChange={draw_image} id="fileIn" accept="image/jpeg, image/png, image/jpg"></input>
+          <label>Resolution X:</label>
+          <input type="number"></input>
+          <label>Resolution Y:</label>
+          <input type="number"></input>
+          <select>
+            <option value="RGB-8">RGB 8-bit</option>
+          </select>
+          <input type="textfield" ></input>
+        </div>
+      </div>
     </div>
   );
 }
